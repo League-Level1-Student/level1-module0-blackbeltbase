6b81a9d0b89499ba7a37f9f95424e7568595bc06
League Student <league@iMac-14.attlocal.net>
2020-01-20 17:00:20 -0800

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,27 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+		Robot houseDrawer = new Robot();
+		houseDrawer.penDown();
+		houseDrawer.moveTo(0, 250);
+		for(int i = 0; i<=8; i++) {
+			houseDrawer.setRandomPenColor();
+			houseDrawer.turn(-90);
+			if(i%2 == 2) {
+				houseDrawer.move(150);
+				houseDrawer.turn(90);
+				houseDrawer.move(30);
+				houseDrawer.turn(90);
+				houseDrawer.move(150);
+			}
+			else {
+// draw that weird triangle house here
+			}
+			houseDrawer.turn(-90);
+			houseDrawer.move(30);
+		}
 	}
 }
